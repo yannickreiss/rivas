df779da0058a3bf46d1239d14aacc0945b6b243f
yreis001 <yannick.reiss@student.hs-rm.de>
2023-01-20 13:09:16 +0100

    checking command line arguments

--- Rust/rivas/src/main.rs
+++ Rust/rivas/src/main.rs
@@ -1,6 +1,20 @@
+use core::panic;
 use std::env;
 
 fn main() {
+
+    // load command line arguments
     let argv: Vec<String> = env::args().collect();
+    let argc: usize = argv.len();
+
+    // check if number of supplied arguments is alright
+    if argc < 1 {
+        panic!("Missing Arguments!");
+    }
+
+    if argc % 2 == 1 {
+        panic!("Wrong number of arguments given!");
+    }
+
     
 }
